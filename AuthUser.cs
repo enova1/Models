441804f685d36da130548693dc13e8df0a6d20e7
enova1 <xxclearwaterxx@gmail.com>
2024-04-08 17:53:28 -0400

    first commit

---
+++ AuthUser.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models;
+
+/// <summary>
+/// Authorized User model for the AuthUser view.
+/// </summary>
+public class AuthUser
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    [Required]
+    public string? FirstName { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    [Required]
+    public string? LastName { get; set; }
+    public string? Password { get; set; }
+    public DateTime RegistrationDate{ get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? City { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")]
+    [Required]
+    public string? PostCode { get; set; }
+}
