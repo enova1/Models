23f9d6f7ddf3afc8cbcd773c79732f03ff0b04f4
enova1 <xxclearwaterxx@gmail.com>
2024-05-01 16:37:15 -0400

    added hangfire

---
+++ Hangfire/HangfireRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Models.Hangfire;
+
+public class HangfireRequest
+{
+    public string JobName { get; set; }
+    public RecurringOption CronExpression { get; set; }
+
+
+}
---
+++ Hangfire/RecurringOption.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire;
+
+namespace Models.Hangfire;
+
+public enum RecurringOption
+{
+    Minutely,
+    Hourly,
+    Daily,
+    Weekly,
+    Monthly,
+    Yearly
+}
+
